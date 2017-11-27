6bfa07a13c0facbd9834a41ed573ad447ca3ddd9
Attila Gal <attila.gal@digipolis.be>
2017-11-27 08:26:58 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parserOptions": { "ecmaVersion": 8 },
+    "env": {
+        "es6": true,
+    },
+    "rules": {
+        "no-param-reassign": "off",
+    }
+};
